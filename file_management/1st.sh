29effe4a691044366101a22798e7996e95847f2c
Raja Prasad <113759499+merajaprasad@users.noreply.github.com>
2024-10-25 23:00:26 +0530

    Update 1st.sh

--- file_management/1st.sh
+++ file_management/1st.sh
@@ -9,7 +9,6 @@ if ! cd scripts > /dev/null 2>&1; then
    echo "scripts directory is not present. hence creating"
 fi
 
-
 # creating directory
 create_dir() {
 
@@ -19,13 +18,37 @@ create_dir() {
    sleep 5
 
 }
-
 create_dir
-
 
 # deleting directory
 delete_dir() {
    rm -rf scripts/ 
 }
-
 delete_dir
+
+------------------------------------------------------------------
+# another way
+
+#!/bin/bash
+set -euo pipefail
+
+check_dir() {
+   if [ ! -d script ]; then
+      echo "script directory is not present, hence creating"
+
+      mkdir script
+      echo "directory is created now"
+
+   else
+      echo "directory is already present, do you want to delete"
+      read -p "type 'ok' if you want to delete: " ok
+      if [ "$ok" == "ok" ]; then
+         rm -rf script/
+         echo "directory is deleted"
+      else
+         echo "fine then directory is not removed"
+      fi
+   fi
+}
+check_dir
+
