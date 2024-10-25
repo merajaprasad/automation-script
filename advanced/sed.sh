b0f2c47e55e6a812d68e3538eb1db972a4aafaaf
merajaprasad <prasad.rp731@gmail.com>
2024-10-25 20:44:21 +0530

    adding sed file

--- advanced/sed.sh
+++ advanced/sed.sh
@@ -5,14 +5,35 @@ set -euo pipefail
 echo "hello" | sed 's/hello/goodbye/'
 
 git_clone() {
-   git clone https://github.com/merajaprasad/wanderlust-project.git
+   if [ -d wanderlust-project ]; then
+      echo "directory is already present"
+   else
+      echo "cloning git repo"
+      git clone https://github.com/merajaprasad/wanderlust-project.git
+   
+   fi
 }
-
 git_clone
 
-if git_clone; then
-   echo "directory is already present"
-   cd ./wanderlust-project/kubermnetes/
-   sed -i 's/wanderlust:latest/wanderlust:oldest/' deployment.yaml
-   cat deployment.yaml
+
+
+changing_manifest() {
+   if [ -d wanderlust-project ]; then
+      cd ./wanderlust-project/kubernetes/
+
+      echo "checking old image version"
+      grep 'image:' frontend.yaml | awk -F: '{print $3}'
+      sleep 2
+
+      echo "updating with new image version"
+      sed -i 's/wonderlust-front.*/wonderlust-front:v2.1.2/' frontend.yaml
+      grep 'image:' frontend.yaml | awk -F: '{print $3}'
+   else
+      echo "ERROR: wanderlust-project is not present"
+   
+   fi
+}
+
+changing_manifest
+
 
