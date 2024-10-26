645c395814eb264de9babed87d4df91802b17bf0
Raja Prasad <113759499+merajaprasad@users.noreply.github.com>
2024-10-26 10:34:27 +0530

    Update 3rd.sh

--- file_management/3rd.sh
+++ file_management/3rd.sh
@@ -15,6 +15,34 @@ task
 #    sudo cp /var/www/html/*  /backup/html/
 
 # }
+-----------------------------------------------------------------------------
+backup_script() {
+
+   DEST_DIR="/backup/html"
+
+   # check if the destination directory exist
+   if [ ! -d $DEST_DIR ]; then
+      sudo mkdir -p $DEST_DIR
+   else
+      echo "$DEST_DIR directory already exist"
+      sleep 2   
+   fi
+
+   # copy the files
+   echo "Taking Backups"
+   cd /var/www/ && sudo tar -czvf "/backup/html/backup_one.tar.gz"  html/
+
+   if [ $? -eq 0 ]; then
+      echo "Backup Successful"
+   else
+      echo "Backup Faild"
+      exit 1
+   fi
+}
+
+backup_script
+--------------------------------------------------------------------------------
+
 
 backup_script() {
 
